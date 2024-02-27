24d607268b36d264d45f5c0664655a2059911dcd
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-27 15:36:36 +0100

    function in functions.php and base structure with bootstrap&print on page

--- functions.php
+++ functions.php
@@ -1,1 +1,12 @@
 <?php
+
+function generateRandomPassword($length)
+{
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|[]\;\',./<>?:';
+    $password = '';
+    $charsLength = strlen($chars);
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[rand(0, $charsLength - 1)];
+    }
+    return $password;
+}
--- index.php
+++ index.php
@@ -10,7 +10,41 @@
 </head>
 
 <body>
+    <div class="container">
+        <h2 class="mt-5 mb-4">Password Generator</h2>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <!-- get method -->
+                <form action="index.php" method="GET">
+                    <div class="form-group">
+                        <label for="password_length">Password Length:</label>
+                        <input type="number" class="form-control" id="password_length" name="password_length" min="1" max="50" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Generate Password</button>
+                </form>
 
+                <?php
+
+                //include del file functions
+                include 'functions.php';
+
+                //controllo del set di password length e get del dato, per poi usare la funzione e generarla
+                if (isset($_GET['password_length'])) {
+
+                    //get dal input type con name password_length
+                    $passwordLength = $_GET['password_length'];
+
+                    //funzione in functions.php e valore ricavato dall'input nelle tonde
+                    $password = generateRandomPassword($passwordLength);
+                    echo "<div class='mt-4'>Your random password: <strong>$password</strong></div>";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
